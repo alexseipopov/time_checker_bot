9c857c9e1fc807f40b2fb6494d8a920d7ac5a873
Alexsei Popov <popov2895@gmail.com>
2023-08-31 16:23:30 +0300

    add logic of inserting

--- database_handlers/db.py
+++ database_handlers/db.py
@@ -11,10 +11,11 @@ async def get_id_by_tg_id(tg_id):
     try:
         pool = await asyncpg.create_pool(db_connection)
         async with pool.acquire() as connection:
-            id = await connection.fetchval(f'''
+            id = await connection.fetch(f'''
                 SELECT id FROM users WHERE user_id = {tg_id}
             ''')
-        return dict(id)['id']
+            logging.info(f"User {tg_id} has id {id}")
+        return id[0]['id']
     except Exception as e:
         logging.exception(e)
         return None
@@ -24,34 +25,61 @@ async def get_last_period_by_id(user_id):
     try:
         pool = await asyncpg.create_pool(db_connection)
         async with pool.acquire() as connection:
-            last_period = await connection.fetchval(f'''
+            last_period = await connection.fetch(f'''
                 SELECT id FROM periods WHERE user_id = {user_id} AND end_date is NULL ORDER BY id DESC LIMIT 1
             ''')
             if not last_period:
                 await connection.execute(f'''
                     INSERT INTO periods (user_id) VALUES ({user_id})
                 ''')
-                last_period = await connection.fetchval(f'''
+                last_period = await connection.fetch(f'''
                     SELECT id FROM periods WHERE user_id = {user_id} ORDER BY id DESC LIMIT 1
                 ''')
-        return dict(last_period)['id']
+        return last_period[0]['id']
     except Exception as e:
         logging.exception(e)
         return None
 
 
 async def insert_new_hours_count(tg_id, hours_count, date):
+    logging.info(f"Inserting new hours count: {tg_id}, {hours_count}, {date}")
+    try:
+        pool = await asyncpg.create_pool(db_connection)
+        async with pool.acquire() as connection:
+            user_id = await get_id_by_tg_id(tg_id)
+            period_id = await get_last_period_by_id(user_id)
+            date_check = await connection.fetch(f'''
+                SELECT id FROM hours WHERE user_id = {user_id} AND period_id = {period_id} AND date = '{date}'
+            ''')
+            if date_check:
+                logging.info(f"User {tg_id} already have hours count for {date}")
+                return False
+            await connection.execute(f'''
+                INSERT INTO hours (user_id, period_id, date, hours_count) 
+                VALUES ({user_id}, {period_id}, '{date}', {hours_count})
+            ''')
+            logging.info(f"User {tg_id} successfully inserted new hours count for {date}")
+            return True
+    except Exception as e:
+        logging.exception(e)
+        return False
+
+
+async def update_description(tg_id, description, date):
+    logging.info(f"Updating description: {tg_id}, {description}, {date}")
     try:
         pool = await asyncpg.create_pool(db_connection)
         async with pool.acquire() as connection:
             user_id = await get_id_by_tg_id(tg_id)
             period_id = await get_last_period_by_id(user_id)
             await connection.execute(f'''
-                INSERT INTO hours (user_id, period_id, date, hours_count) 
-                VALUES ({user_id}, {period_id}, {date}, {hours_count})
+                UPDATE hours SET description = '{description}' WHERE user_id = {user_id} AND period_id = {period_id} AND date = '{date}'
             ''')
+            logging.info(f"User {tg_id} successfully updated description for {date}")
+            return True
     except Exception as e:
         logging.exception(e)
+        return False
 
 
 async def close_period(tg_id):
@@ -73,7 +101,7 @@ async def insert_new_user(tg_id):
         pool = await asyncpg.create_pool(db_connection)
         async with pool.acquire() as connection:
             user_id = await connection.fetch(f'''
-                SELECT id FROM users WHERE user_id = {tg_id})
+                SELECT id FROM users WHERE user_id = {tg_id}
             ''')
             if not user_id:
                 await connection.execute(f'''
--- main.py
+++ main.py
@@ -9,9 +9,11 @@ from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from database_handlers.db import insert_new_user
+from database_handlers.db import insert_new_user, insert_new_hours_count, update_description
+
+storage = MemoryStorage()
 
 API_TOKEN = os.getenv("TOKEN")
 
@@ -20,7 +22,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 dp.middleware.setup(LoggingMiddleware())
 
 menu_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -45,6 +47,51 @@ async def cmd_start(message: types.Message):
         await message.answer("Привет! Я буду присылать тебе опросы о потраченном времени!", reply_markup=menu_keyboard)
     else:
         await message.answer("Привет! Ты уже зарегистрирован!", reply_markup=menu_keyboard)
+
+
+@dp.message_handler(Text(equals=["Зафиксировать время", "Зафиксировать задачи", "Закрыть период"]))
+async def get_poll(message: types.Message, state: FSMContext):
+    if message.text == "Зафиксировать время":
+        await UserStates.waiting_for_time.set()
+        await message.answer("Фиксируем время. Введите время целым числом:")
+    elif message.text == "Зафиксировать задачи":
+        await UserStates.waiting_for_tasks.set()
+        await message.answer("Фиксируем задачи. Опишите одним сообщением задачи за день:")
+    elif message.text == "Закрыть период":
+        await message.answer("Составляем отчет. Введите отчет:")
+    else:
+        await message.answer("Что-то пошло не так. Попробуйте еще раз.")
+
+
+@dp.message_handler(state=UserStates.waiting_for_time)
+async def process_input(message: types.Message, state: FSMContext):
+    try:
+        target = int(message.text)
+        prev_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+        res = await insert_new_hours_count(message.from_user.id, target, prev_date)
+        if res:
+            await message.answer(f"Ответ успешно сохранен")
+        else:
+            await message.answer(f"Ответ не сохранен")
+    except ValueError:
+        await message.answer("Пожалуйста, введи число!")
+        return
+    await state.finish()
+
+
+@dp.message_handler(state=UserStates.waiting_for_tasks)
+async def process_input(message: types.Message, state: FSMContext):
+    try:
+        prev_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+        res = await update_description(message.from_user.id, message.text, prev_date)
+        if res:
+            await message.answer(f"Ответ успешно сохранен")
+        else:
+            await message.answer(f"Ответ не сохранен")
+    except ValueError:
+        await message.answer("Пожалуйста, введи число!")
+        return
+    await state.finish()
 
 
 @dp.message_handler(content_types=types.ContentType.TEXT)
